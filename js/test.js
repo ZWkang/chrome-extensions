4a2f27c46b8b64824cc7d978e05e30d2ae699f28
ZWkang <907747874@qq.com>
2016-08-22 23:16:43 +0800

    chrome extensions

---
+++ js/test.js
@@ -0,0 +1,66 @@
+function biquge(){
+	if(window.location.host==='www.biquge.la'){
+		$("iframe").remove();
+	}
+}
+function dytt(){
+	if(window.location.host==='www.ygdy8.net'){
+		// var aa =document.getElementsByTagName("a")[0]
+		// aa.parentNode.removeChild(aa);
+		$("a")[0].remove();
+		$("iframe").remove();
+	}
+}
+function mp4ba(){
+	if(window.location.host==='www.mp4ba.com'){
+		// var cs_left_couplet = document.getElementById("cs_left_couplet");
+		// var cs_right_couplet = document.getElementById("cs_right_couplet");
+		// var cs_right_bottom = document.getElementById("cs_right_bottom");
+		// cs_left_couplet.parentNode.removeChild(cs_left_couplet);
+		// cs_right_couplet.parentNode.removeChild(cs_right_couplet);
+		// var iframes = document.getElementsByTagName("iframe");
+		// for(var i=iframes.length-1;i>=0;i--){
+		// 	iframes[i].parentNode.removeChild(iframes[i]);
+		// }
+		// var ad_list_middle = document.getElementById("ad_list_middle");
+		// if (ad_list_middle!==undefined){
+		// 	ad_list_middle.parentNode.removeChild(ad_list_middle);
+		// }
+		// cs_right_bottom.parentNode.removeChild(cs_right_bottom);
+		$("#cs_left_couplet").remove();
+		$("#cs_right_couplet").remove();
+		$("#cs_right_bottom").remove();
+		$("#cs_right_bottom").remove();
+		$("iframe").remove();
+	}
+}
+function loldytt(){
+	if(window.location.host==='www.loldytt.com'){
+		
+		$("#qj960a").remove();
+		$("#xinnxi").remove();
+		$("#xinxi").remove();
+		$("iframe").remove();
+		$("#xinxi").remove();
+		$("#cs_right_bottom").remove();
+	}
+}
+function banyungong(){
+	if(window.location.host==='banyungong.net'){
+		$("iframe").remove();
+		$("#ggDiv2").remove();
+		// $("aswift_0_expand").remove();
+	}
+}
+$(function(){
+	// alert('123123');
+	var func = Array('mp4ba()','loldytt()','dytt()','banyungong()','biquge()');
+	var aaa = Array('www.mp4ba.com','www.loldytt.com','www.ygdy8.net','banyungong.net','www.biquge.la');
+	var numbers = $.inArray(window.location.host,aaa);
+	if (numbers>=0){
+		str = func[numbers];
+		// alert(str);
+		eval(str);
+		// alert(str);
+	}
+})
